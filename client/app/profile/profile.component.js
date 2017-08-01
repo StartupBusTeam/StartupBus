4d2e6f85a07a39e0391bd4b38164fcdda4115ca1
James Wesley Goedert <jwgoedert@gmail.com>
2017-08-01 03:05:54 -0500

    (bugfix)Save before removing profile directory

--- client/app/profile/profile.component.js
+++ client/app/profile/profile.component.js
@@ -1,16 +1,33 @@
 'use strict';
 const angular = require('angular');
-
 const uiRouter = require('angular-ui-router');
 
 import routes from './profile.routes';
 
 export class ProfileComponent {
+  $http;
+  socket;
+
+  newTrade;
+  newLocation;
+  newTelephone;
+  newKeywords = [];
+  newPhoto;
+  newBio;
+
   /*@ngInject*/
-  constructor() {
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.socket = socket;
     this.message = 'Hello';
   }
+
+  addProfile() {
+    console.log("this.newTrade, this.newLocation")
+  }
+
 }
+
 
 export default angular.module('startupbusApp.profile', [uiRouter])
   .config(routes)
